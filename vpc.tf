afbfeed2de65f9177318c43a5975862ada93dad9
michaelkev <95002714+michaelkev@users.noreply.github.com>
2022-03-01 21:53:24 -0500

    this is my second commit

--- vpc.tf
+++ vpc.tf
@@ -1,68 +1,63 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
 # Creating VPC,name, CIDR and Tags
-resource "aws_vpc" "dev" {
-  cidr_block           = "${var.vpc-cidr}"
-  instance_tenancy     = "default"
-  enable_dns_support   = "true"
-  enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
+resource "aws_vpc" "dev_vpc" {
+  cidr_block = "192.168.0.0/16"
   tags = {
-    Name = "dev"
+    Name = "dev_vpc"
   }
 }
 # Creating Internet Gateway in AWS VPC
-resource "aws_internet_gateway" "dev-gw" {
-  vpc_id = aws_vpc.dev.id
-
+resource "aws_internet_gateway" "dev_igw" {
+  vpc_id = aws_vpc.dev_vpc.id
   tags = {
     Name = "dev IGW"
   }
 }
 # Creating Public Subnets in VPC
 resource "aws_subnet" "dev-public-1" {
-  vpc_id                  = aws_vpc.dev.id
-  cidr_block              = "${var.public-subnet-1-cird}"
-  map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-1a"
-
+  vpc_id            = aws_vpc.dev_vpc.id
+  cidr_block        = "192.168.0.0/24"
+  availability_zone = "us-east-1a"
   tags = {
     Name = "dev-public-1"
   }
 }
 # Creating Private Subnets in VPC
 resource "aws_subnet" "dev-private-1" {
-  vpc_id                  = aws_vpc.dev.id
-  cidr_block              = "${var.public-subnet-1-cird"
-  map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-1"
-
+  vpc_id            = aws_vpc.dev_vpc.id
+  cidr_block        = "192.168.2.0/24"
+  availability_zone = "us-east-1b"
   tags = {
     Name = "dev-private-1"
   }
 }
 
 # Creating Route Tables for Internet gateway
-resource "aws_route_table" "dev-public" {
-  vpc_id = aws_vpc.dev.id
+resource "aws_route_table" "dev-public_rt" {
+  vpc_id = aws_vpc.dev_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_gateway.dev-gw.id"
+    gateway_id = aws_internet_gateway.dev_igw.id
   }
-
   tags = {
-    Name = "dev-public route aws_route_table-1"
+    Name = "dev-public route"
   }
 }
 # Creating Route Associations public subnets
-resource "aws_route_table_association" "dev-public-1-a" {
+resource "aws_route_table_association" "dev_public_rta" {
   subnet_id      = aws_subnet.dev-public-1.id
-  route_table_id = aws_route_table.dev-public.id
+  route_table_id = aws_route_table.dev-public_rt.id
 }
-# Creating Route Associations private subnets
-resource "aws_route_table" "dev-private" {
-  vpc_id = aws_vpc.dev.id
+# Creating Route Table private subnets
+resource "aws_route_table" "dev-private_rt" {
+  vpc_id = aws_vpc.dev_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_gateway.dev-gw.id"
+    gateway_id = aws_nat_gateway.dev-natgw.id
   }
 
   tags = {
@@ -72,29 +67,35 @@ resource "aws_route_table" "dev-private" {
 # Creating Route Associations private subnets
 resource "aws_route_table_association" "dev-private-1-a" {
   subnet_id      = aws_subnet.dev-private-1.id
-  route_table_id = aws_route_table.dev-private.id
-
+  route_table_id = aws_route_table.dev-private_rt.id
+}
+resource "aws_nat_gateway" "dev-natgw" {
+  subnet_id     = aws_subnet.dev-private-1.id
+  allocation_id = aws_eip.dev-eip.id
+}
+resource "aws_eip" "dev-eip" {
+  vpc = true
+}
 
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow ssh inbound traffic"
-  vpc_id      = aws_vpc.dev.id
-
+  vpc_id      = aws_vpc.dev_vpc.id
+  #
   ingress {
-    description      = "ssh from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.dev.cidr_block]
-    
+    description = "ssh from internet"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   tags = {
@@ -105,23 +106,23 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_security_group" "allow_http" {
   name        = "allow_http"
   description = "Allow https inbound traffic"
-  vpc_id      = aws_vpc.dev.id
+  vpc_id      = aws_vpc.dev_vpc.id
 
   ingress {
-    description      = "http from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.dev.cidr_block]
-    
+    description = "http from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   tags = {
@@ -129,63 +130,28 @@ resource "aws_security_group" "allow_http" {
   }
 }
 resource "aws_security_group" "allow_https" {
-  name        = "allow_http"
+  name        = "allow_https"
   description = "Allow https inbound traffic"
-  vpc_id      = aws_vpc.dev.id
+  vpc_id      = aws_vpc.dev_vpc.id
 
   ingress {
-    description      = "https from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.dev.cidr_block]
-    
+    description = "allow https"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   tags = {
     Name = "allow_https"
-  }
-}
-#create load balancer
-resource "aws_elb" ""aws_vpc" "dev" {
-  name               = ""aws_vpc" "dev"
-  availability_zones = ["us-east-1a"]
-
-  listener {
-    instance_port      = 80
-    instance_protocol  = "http"
-    lb_port            = 80
-    lb_protocol        = "http"
-    ssl_certificate_id = "arn:aws:iam::000000000000:server-certificate/wu-tang.net"
-  }
-
-  tags = {
-    Name = ""aws_vpc" "dev"
-  }
-} 
-
-
-resource "aws_elb" ""aws_vpc" "dev" {
-  name               = ""aws_vpc" "dev"
-  availability_zones = ["us-east-1a"]
-
-  listener {
-    instance_port      = 443
-    instance_protocol  = "https"
-    lb_port            = 443
-    lb_protocol        = "https"
-    ssl_certificate_id = "arn:aws:iam::000000000000:server-certificate/wu-tang.net"
-  }
-
-  tags = {
-    Name = ""aws_vpc" "dev"
   }
 }
